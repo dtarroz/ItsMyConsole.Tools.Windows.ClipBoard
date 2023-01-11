fc8a0ee912369d606f5ea3eba1c580a0a2c583bd
dtarroz <damien.tarroz.apps@valsoftware.com>
2023-01-11 08:18:42 +0100

    feat: add get and set text as html

--- src/ItsMyConsole.Tools.Windows.Clipboard/Services/ClipboardTools.cs
+++ src/ItsMyConsole.Tools.Windows.Clipboard/Services/ClipboardTools.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Windows.Forms;
 
 namespace ItsMyConsole.Tools.Windows.Clipboard
 {
@@ -15,13 +16,23 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
         public string GetText()
         {
             string text = null;
-            Thread thread = new Thread(() =>
+            RunThread(() =>
             {
                 text = System.Windows.Forms.Clipboard.GetText();
             });
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-            thread.Join();
+            return text;
+        }
+
+        /// <summary>
+        /// Récupérer le texte du presse papier en tant que HTML
+        /// </summary>
+        public string GetTextAsHtml()
+        {
+            string text = null;
+            RunThread(() =>
+            {
+                text = System.Windows.Forms.Clipboard.GetText(TextDataFormat.Html);
+            });
             return text;
         }
 
@@ -31,13 +42,47 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
         /// <param name="text">Le texte à mettre dans le presse papier</param>
         public void SetText(string text)
         {
-            Thread thread = new Thread(() =>
+            RunThread(() =>
             {
                 System.Windows.Forms.Clipboard.SetText(text);
             });
+        }
+
+        /// <summary>
+        /// Mettre un texte dans le presse papier en tant que HTML
+        /// </summary>
+        /// <param name="htmlText">Le texte à mettre dans le presse papier en tant que HTML</param>
+        /// <param name="plainText">Le texte "plat" à mettre dans le presse papier pour les logiciels qui ne gére pas le texte en tant que HTML</param>
+        public void SetTextAsHtml(string htmlText, string plainText)
+        {
+            string htmlClipboard = ConvertToHtmlClipboard(htmlText);
+            RunThread(() =>
+            {
+                DataObject dataObject = new DataObject();
+                dataObject.SetData(DataFormats.Html, htmlClipboard);
+                dataObject.SetData(DataFormats.Text, plainText);
+                System.Windows.Forms.Clipboard.SetDataObject(dataObject, true);
+            });
+        }
+
+        private void RunThread(ThreadStart start)
+        {
+            Thread thread = new Thread(start);
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
             thread.Join();
+        }
+
+        private string ConvertToHtmlClipboard(string text)
+        {
+            string header = "Version:0.9\r\nStartHTML:0000000000\r\nEndHTML:0000000000\r\nStartFragment:0000000000\r\nEndFragment:0000000000\r\n";
+            string htmlStart = "<html>\r\n<body>\r\n<!--StartFragment-->";
+            string htmlEnd = "<!--EndFragment-->\r\n</body>\r\n</html>";
+            string html = header.Replace("StartHTML:0000000000", $"StartHTML:{header.Length:0000000000}") + htmlStart;
+            html = html.Replace("StartFragment:0000000000", $"StartFragment:{html.Length:0000000000}") + text;
+            html = html.Replace("EndFragment:0000000000", $"EndFragment:{html.Length:0000000000}") + htmlEnd;
+            html = html.Replace("EndHTML:0000000000", $"EndHTML:{html.Length:0000000000}");
+            return html;
         }
     }
 }
