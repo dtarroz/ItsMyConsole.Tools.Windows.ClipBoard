7329ec1c3d41733ffbd27ca8dcf1cd6e6a5bea09
dtarroz <damien.tarroz.apps@gmail.com>
2023-01-12 07:35:20 +0100

    refactor: clipboard tools

--- src/ItsMyConsole.Tools.Windows.Clipboard/Services/ClipboardTools.cs
+++ src/ItsMyConsole.Tools.Windows.Clipboard/Services/ClipboardTools.cs
@@ -13,11 +13,9 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
         /// <summary>
         /// Récupérer le texte du presse papier
         /// </summary>
-        public string GetText()
-        {
+        public string GetText() {
             string text = null;
-            RunThread(() =>
-            {
+            RunThread(() => {
                 text = System.Windows.Forms.Clipboard.GetText();
             });
             return text;
@@ -26,11 +24,9 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
         /// <summary>
         /// Récupérer le texte du presse papier en tant que HTML
         /// </summary>
-        public string GetTextAsHtml()
-        {
+        public string GetTextAsHtml() {
             string text = null;
-            RunThread(() =>
-            {
+            RunThread(() => {
                 text = System.Windows.Forms.Clipboard.GetText(TextDataFormat.Html);
             });
             return text;
@@ -40,10 +36,8 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
         /// Mettre un texte dans le presse papier
         /// </summary>
         /// <param name="text">Le texte à mettre dans le presse papier</param>
-        public void SetText(string text)
-        {
-            RunThread(() =>
-            {
+        public void SetText(string text) {
+            RunThread(() => {
                 System.Windows.Forms.Clipboard.SetText(text);
             });
         }
@@ -53,11 +47,9 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
         /// </summary>
         /// <param name="htmlText">Le texte à mettre dans le presse papier en tant que HTML</param>
         /// <param name="plainText">Le texte "plat" à mettre dans le presse papier pour les logiciels qui ne gére pas le texte en tant que HTML</param>
-        public void SetTextAsHtml(string htmlText, string plainText)
-        {
+        public void SetTextAsHtml(string htmlText, string plainText) {
             string htmlClipboard = ConvertToHtmlClipboard(htmlText);
-            RunThread(() =>
-            {
+            RunThread(() => {
                 DataObject dataObject = new DataObject();
                 dataObject.SetData(DataFormats.Html, htmlClipboard);
                 dataObject.SetData(DataFormats.Text, plainText);
@@ -65,20 +57,20 @@ namespace ItsMyConsole.Tools.Windows.Clipboard
             });
         }
 
-        private void RunThread(ThreadStart start)
-        {
+        private static void RunThread(ThreadStart start) {
             Thread thread = new Thread(start);
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
             thread.Join();
         }
 
-        private string ConvertToHtmlClipboard(string text)
-        {
-            string header = "Version:0.9\r\nStartHTML:0000000000\r\nEndHTML:0000000000\r\nStartFragment:0000000000\r\nEndFragment:0000000000\r\n";
-            string htmlStart = "<html>\r\n<body>\r\n<!--StartFragment-->";
-            string htmlEnd = "<!--EndFragment-->\r\n</body>\r\n</html>";
-            string html = header.Replace("StartHTML:0000000000", $"StartHTML:{header.Length:0000000000}") + htmlStart;
+        private static string ConvertToHtmlClipboard(string text) {
+            const string version = "Version:0.9\r\n";
+            const string position = "StartHTML:0000000000\r\nEndHTML:0000000000\r\nStartFragment:0000000000\r\nEndFragment:0000000000\r\n";
+            const string htmlStart = "<html>\r\n<body>\r\n<!--StartFragment-->";
+            const string htmlEnd = "<!--EndFragment-->\r\n</body>\r\n</html>";
+            string html = version + position;
+            html = html.Replace("StartHTML:0000000000", $"StartHTML:{position.Length:0000000000}") + htmlStart;
             html = html.Replace("StartFragment:0000000000", $"StartFragment:{html.Length:0000000000}") + text;
             html = html.Replace("EndFragment:0000000000", $"EndFragment:{html.Length:0000000000}") + htmlEnd;
             html = html.Replace("EndHTML:0000000000", $"EndHTML:{html.Length:0000000000}");
